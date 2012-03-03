8570055864a499903eadd84ff257505513bf6fb4
Charles Hagman <hagmanc@gmail.com>
2012-03-03 11:58:55 -0500

    Updated the license to MIT

--- CHSlideViewController.m
+++ CHSlideViewController.m
@@ -3,8 +3,14 @@
 //  CHSildeViewController
 //
 //  Created by Charles Hagman on 2/25/12.
-//  Copyright (c) 2012 Deloitte. All rights reserved.
-//
+/*  Copyright (C) 2012 Charles Hagman
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+ */
 
 #import "CHSlideViewController.h"
 #import <QuartzCore/QuartzCore.h>
@@ -70,6 +76,7 @@
             detailViewController.view.frame = moveFrameTo;
         }
         completion:^(BOOL finished){
+            //When finished the animation add the gesture to move the view back into place
             UIViewController *detailViewController = [self detailViewController];
             [detailViewController.view addGestureRecognizer:self.tapGesture];
         }];
@@ -83,6 +90,7 @@
             detailViewController.view.frame = moveFrameTo;
         } 
         completion:^(BOOL finished){
+            //Remove the gesture so the user has to hit the UIBarButton to toggle the view
             UIViewController *detailViewController = [self detailViewController];
             [detailViewController.view removeGestureRecognizer:self.tapGesture];
         }];
@@ -149,8 +157,8 @@
     
     //Add a shadow on the top view controller
     detailController.view.layer.masksToBounds = NO;
-    detailController.view.layer.shadowOffset = CGSizeMake(-3, 0);
-    detailController.view.layer.shadowOpacity = 0.3;
+    detailController.view.layer.shadowOffset = CGSizeMake(-2, 0);
+    detailController.view.layer.shadowOpacity = 0.4;
     
     [self.view addSubview:detailController.view];
     
